7d2fc965b618d33744535c57d1ff5644ffd9b351
Kaumba Chinyama <kaumba@kalichipictures.com>
2024-08-05 14:12:08 -0700

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,25 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
-@app_views.route('/status')
+@app_views.route('/status', methods=['GET'])
 def myStatus():
     """returns ok status message"""
 
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def objStats():
+    """retrieves agregated stats for all objs by class name"""
+
+    stats = {
+            "amenities": storage.count('Amenity'),
+            "cities": storage.count('City'),
+            "places": storage.count('Place'),
+            "reviews": storage.count('Review'),
+            "states": storage.count('State'),
+            "users": storage.count('User')
+        }
+
+    return jsonify(stats)
