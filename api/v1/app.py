63c9209dacbe7ad02d6354c90132a5e06cba4caf
Kaumba Chinyama <kaumba@kalichipictures.com>
2024-08-06 03:31:36 -0700

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,11 +7,19 @@ from os import getenv
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask import make_response
+from flask import jsonify
+
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+@app.errorhandler(404)
+def errorHandle(error):
+    """takes and handles 404 errors with custom message"""
+
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
