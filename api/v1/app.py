774d2eb10de2775335242512e6e372801db4de44
Kaumba Chinyama <kaumba@kalichipictures.com>
2024-08-06 03:35:10 -0700

    task 5-pycodestyle corrections

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,6 +15,7 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.errorhandler(404)
 def errorHandle(error):
     """takes and handles 404 errors with custom message"""
@@ -23,6 +24,6 @@ def errorHandle(error):
 
 
 if __name__ == "__main__":
-    HOST=getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT=int(getenv('HBNB_API_PORT', 5000))
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=HOST, port=PORT, threaded=True)
